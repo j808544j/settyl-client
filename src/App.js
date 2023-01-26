9f85f88aa20ea68614b654e87862289f701d67f6
j808544j <j808544j@gmail.com>
2023-01-26 13:35:06 +0530

    gt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
+import CitiesList from "./CitiesList";
+import "./App.css";
 
 function App() {
+  const [selectedCity, setSelectedCity] = useState(null);
+  const [map, setMap] = useState(null);
+
+  useEffect(() => {
+    map?.flyTo(
+      selectedCity
+        ? [selectedCity?.coord.lat, selectedCity?.coord.lon]
+        : [51.505, -0.09],
+      14,
+      {
+        duration: 3,
+      }
+    );
+  }, [selectedCity, map]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <MapContainer
+        center={
+          selectedCity
+            ? [selectedCity?.coord?.lat, selectedCity?.coord?.lon]
+            : [51.505, -0.09]
+        }
+        zoom={13}
+        scrollWheelZoom={false}
+        ref={setMap}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker
+          position={
+            selectedCity
+              ? [selectedCity?.coord?.lat, selectedCity?.coord?.lon]
+              : [51.505, -0.09]
+          }
         >
-          Learn React
-        </a>
-      </header>
+          <Popup>
+            {selectedCity?.name} <br /> {selectedCity?.main.temp} <br />{" "}
+            {selectedCity?.weather[0].description}
+          </Popup>
+        </Marker>
+      </MapContainer>
+      <CitiesList
+        selectedCity={selectedCity}
+        setSelectedCity={setSelectedCity}
+      />
     </div>
   );
 }
---
+++ src/CitiesList.js
@@ -0,0 +1,109 @@
+import { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
+
+function CitiesList({ selectedCity, setSelectedCity }) {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [noOfPages, setNoOfPages] = useState(0);
+  const [weatherData, setWeatherData] = useState(null);
+  const [selectedIndex, setSelectedIndex] = useState(1);
+  const [refetch, setRefetch] = useState(false);
+
+  useEffect(() => {
+    fetch(`/weather?page=${selectedIndex}`)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setNoOfPages(JSON.parse(result.noOfPages));
+          setWeatherData(parseData(result));
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, [selectedIndex, refetch]);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setRefetch((prev) => !prev);
+    }, 5 * 60 * 5000);
+
+    return () => clearInterval(id);
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else if (weatherData) {
+    return (
+      <div style={style3}>
+        <ul style={style1}>{citiesList(weatherData, setSelectedCity)}</ul>
+        <div style={style2}>{pageIndexes(noOfPages, setSelectedIndex)}</div>
+        <div>Selected city : {selectedCity?.name}</div>
+        <div>Selected Index : {selectedIndex}</div>
+      </div>
+    );
+  }
+}
+
+const style1 = {
+  display: "flex",
+  gap: "5rem",
+  flexWrap: "wrap",
+  cursor: "Pointer",
+};
+
+const style2 = {
+  display: "flex",
+  gap: "1rem",
+  flexWrap: "wrap",
+  listStyle: "none",
+};
+const style3 = {
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  flexDirection: "column",
+};
+
+const parseData = (result) => {
+  return result.data.map((cityWeather) => {
+    const obj = JSON.parse(cityWeather.text);
+    obj.id = nanoid();
+    return obj;
+  });
+};
+
+const pageIndexes = (noOfPages, setSelectedIndex) => {
+  let pageIndexList = [];
+  for (let index = 0; index < noOfPages; index++) {
+    pageIndexList.push(
+      <button
+        id={nanoid()}
+        onClick={() => {
+          setSelectedIndex(index + 1);
+        }}
+      >
+        {index + 1}
+      </button>
+    );
+  }
+  return pageIndexList;
+};
+
+const citiesList = (weatherData, setSelectedCity) => {
+  return weatherData?.map((cityWeather) => (
+    <li
+      id={cityWeather.id}
+      onClick={() => setSelectedCity(cityWeather)}
+      className="city-item"
+    >
+      {cityWeather.name}
+    </li>
+  ));
+};
+
+export default CitiesList;
